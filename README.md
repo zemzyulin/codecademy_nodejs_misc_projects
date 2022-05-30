c024ffb23c7497a363aa0c8c0bce3720cabd54b5
Yehor Zemzyulin <yehor.zemzyulin@gmail.com>
2022-05-30 12:08:58 +0300

    minor update: readme.md

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# Codecademy NodeJS Miscellaneous Projects
+## Codecademy NodeJS Miscellaneous Projects
 
 This repository includes notable mini-projects from Codecademy's Back-End Engineer Career Path: NodeJS.
-I took this 48-module course in early 2022.
+I took this course in early 2022.
+THe course includes: 41 module and 60 mini-projects.
 Here's the link to its syllabus: https://www.codecademy.com/learn/paths/back-end-engineer-career-path
 
-## List of projects (from easy to more complex)
+### List of projects (from easy to more complex)
 1. Mixed Messages
 2. Find The Hat Game
 
--- mixed-messages/mixed_messages.js
+++ mixed-messages/mixed_messages.js
@@ -3,7 +3,7 @@
 let workout = () => {
     const exerciseList = ['Squats', 'Leg press', 'Lunge', 'Deadlift', 'Bench press', 'Push-ups', 'Pull-ups', 'Biceps curl'];
     const exerciseDuration = Math.round((Math.floor(Math.random() * 61) + 30) / 10) * 10;
-    const inspirationList = ['Sweat is just fat!', 'Later = Never. Do it now!', 'The body achieves what the mid believes!',
+    const inspirationList = ['Sweat is just fat!', 'Later = Never. Do it now!', 'The body achieves what the mind believes!',
                             'Tough times don\'t last. Tough people do!', 'Once you learn to quit, it becomes a habit.'];
     return console.log(`Your exercise today: ${exerciseList[Math.floor(Math.random() * 8)]}\nDuration: ${exerciseDuration} seconds\n${inspirationList[Math.floor(Math.random() * 5)]} \nGO!`);         
 }
